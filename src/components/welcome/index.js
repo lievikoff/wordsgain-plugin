14df9b7699853d6bc8122d58a60d22543286b67b
Max Lievikoff <lievikoff@gmail.com>
2020-03-03 18:17:12 +0200

    =Fixed welcome button colors

--- src/components/welcome/index.js
+++ src/components/welcome/index.js
@@ -10,17 +10,15 @@ function Welcome( props ) {
 			</h2>
 
 			<div className={ getElementClassName( 'wordsgain-playground', 'buttons' ) }>
-				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'predefined', numberOfWords: 10 } } color="green" width='half'>{ __( '10 Words', 'wordsgain' ) }</Button>
-				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'predefined', numberOfWords: 20 } } color="green" width='half'>{ __( '20 Words', 'wordsgain' ) }</Button>
-			</div>
-			<div className={ getElementClassName( 'wordsgain-playground', 'buttons' ) }>
+				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'predefined', numberOfWords: 10 } } color="blue" width='half'>{ __( '10 Words', 'wordsgain' ) }</Button>
+				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'predefined', numberOfWords: 20 } } color="blue" width='half'>{ __( '20 Words', 'wordsgain' ) }</Button>
 				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'predefined', numberOfWords: 30 } } color="blue" width='half'>{ __( '30 Words', 'wordsgain' ) }</Button>
 				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'predefined', numberOfWords: 40 } } color="blue" width='half'>{ __( '40 Words', 'wordsgain' ) }</Button>
 				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'predefined', numberOfWords: 50 } } color="blue" width='half'>{ __( '50 Words', 'wordsgain' ) }</Button>
 				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'predefined', numberOfWords: 75 } } color="blue" width='half'>{ __( '75 Words', 'wordsgain' ) }</Button>
 			</div>
 			<div className={ getElementClassName( 'wordsgain-playground', 'buttons' ) }>
-				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'predefined', numberOfWords: 100 } } color="gray" width='wide'>{ __( '100 Words', 'wordsgain' ) }</Button>
+				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'predefined', numberOfWords: 100 } } color="blue" width='wide'>{ __( '100 Words', 'wordsgain' ) }</Button>
 				<Button handleButtonClick={ props.handleButtonClick } data={ { mode: 'free', numberOfWords: getFreeModeNumberOfWords() } } color="dark-gray" width='wide'>{ __( 'Freemode', 'wordsgain' ) }</Button>
 			</div>
 		</div>
